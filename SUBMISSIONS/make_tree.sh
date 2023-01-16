ca73886a4bd00a187bea8c50d6518d6bb6ce790f
jspencermorris <jspencermorris@gmail.com>
2023-01-15 18:04:58 -0600

    added shell script for m01 to the main directory

---
+++ SUBMISSIONS/make_tree.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+mkdir ./s1
+mkdir ./s1/s2
+mkdir ./s1/s2/Advanced
+mkdir ./s1/s3
+cp conf.txt ./s1/s3
+cp text_chunk1.txt ./s1/s2
+cp text_chunk2.txt ./s1/s2/Advanced
+echo 'Directories and Files were created!'
