de4d33930d285f4ab21e880d28f88004b9003ec8
jspencermorris <jspencermorris@gmail.com>
2023-01-16 18:38:58 -0600

    updated capitalization in sh file

--- SUBMISSIONS/m01/make_tree.sh
+++ SUBMISSIONS/m01/make_tree.sh
@@ -4,9 +4,9 @@ mkdir ./s1/s3
 mkdir ./s1/s2
 mkdir ./s1/s2/Advanced
 touch ./s1/s3/conf.txt
-echo 'virtual environments are my favorite new technology' > ./s1/s3/conf.txt
+echo 'Virtual (conda) environments are my favorite new technology.' > ./s1/s3/conf.txt
 touch ./s1/s2/text_chunk1.txt
-echo 'virtual environments are good for managing package dependencies' > ./s1/s2/text_chunk1.txt
+echo 'Virtual environments are good for managing package dependencies.' > ./s1/s2/text_chunk1.txt
 cp ./s1/s2/text_chunk1.txt ./s1/s2/Advanced/text_chunk2.txt
-echo 'I like them because they ensure everything is compatible and keep development organized' >> ./s1/s2/Advanced/text_chunk2.txt
+echo 'I like them because they ensure everything is compatible and keep development organized.' >> ./s1/s2/Advanced/text_chunk2.txt
 echo 'Directories and Files were created!'
