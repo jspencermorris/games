673856e1d322a518ed5245194ea463c0d10455f8
jspencermorris <jspencermorris@gmail.com>
2023-02-19 20:19:17 -0600

    updated comments and formatting

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,5 @@
+from wordscore import score_word
+    
 def run_scrabble(rack):
     """
     Given an input scrabble rack of up to 7 characters...
@@ -27,34 +29,36 @@ def run_scrabble(rack):
         raw_input = infile.readlines()
         sowpods_words = [sowpods_word.strip('\n') for sowpods_word in raw_input]
     
-    # find valid words
+    """
+    find valid words by looping through the list of sowpods words,  
+    creating temporary lists to store chars in the sowpods word and rack,
+    comparing the characters in the sowpod word to the characters in the rack,
+    removing matching characters from the temp lists,
+    and if the number of remaining characters doesn't exceed the number of available wildcards,
+    append the sowpods word to the valid_words list
+    and append the wildcard chars to the invalid_chars list
+    """
     valid_words = [] # initialize an empty list to store all valid words
-    invalid_chars = []
+    invalid_chars = [] # initialize an empty list to store lists chars from wildcards
     for word in sowpods_words:
-        temp_rack = list(rack)
-        temp_word = list(word)
-        #invalid_chars = []
+        temp_rack = list(rack) # for each iter, create a list of chars in the rack
+        temp_word = list(word) # for each iter, create a list of chars in the word
         for letter in word:
+            # remove matching characters from the tempoerary lists
             if letter in temp_rack:
                 temp_rack.remove(letter)
                 temp_word.remove(letter)
+        # check if there are fewer remaining characters than available wildcards
         if len(temp_word) - temp_rack.count('?') - temp_rack.count('*') <= 0:
-            valid_words.append(word)
-            invalid_chars.append(temp_word)
+            valid_words.append(word) # append the word
+            invalid_chars.append(temp_word) # append the list of characters from wildcards
 
-    # step 4 - scoring
-    from wordscore import score_word
-    score_list = []
+    # scoring
+    score_list = [] # create a list to store the words and their scores
+    # calculate the scores of the sowpods word minus the chars from wildcards
     for word,wildcards in zip(valid_words,invalid_chars):
         score_list.append((score_word(word)-score_word(wildcards),word))
     score_list = sorted(score_list, key=lambda option: option[0], reverse=True)
-    #score_list.append(len(score_list))
     valid_num = len(score_list)
-    
-    #print(f'This is finally returned -- \n{score_list}')
-    
-    #import sys
-    #input_rack = sys.argv[1]
-    #run_scrabble(input_rack)
 
     return score_list, valid_num
